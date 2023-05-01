f85326f3ff1e7d840c2a88108d1b89cbf506c555
Didier Malenfant <coding@malenfant.net>
2023-05-01 18:03:58 +0200

    Fixed heder

--- pffpgatools/__init__.py
+++ pffpgatools/__init__.py
@@ -1,3 +1,3 @@
-# SPDX-FileCopyrightText: 2022-present Didier Malenfant
+# SPDX-FileCopyrightText: 2023-present Didier Malenfant
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: GPL-3.0-or-later
